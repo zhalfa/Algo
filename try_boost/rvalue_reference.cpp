6b4036704809728aa9a5aa7f20bfe6eee0a82b2e
Yuan ubuntu20.10 <zhalfa@hotmail.com>
2020-12-06 17:50:41 +0800

    why this workaround can do

--- try_boost/rvalue_reference.cpp
+++ try_boost/rvalue_reference.cpp
@@ -1,6 +1,12 @@
 #include <string>
 #include <vector>
 
+/*
+    constexpr how to use it
+
+https://stackoverflow.com/questions/14116003/difference-between-constexpr-and-const#:~:text=constexpr%20is%20a%20new%20C%2B%2B11%20keyword%20that%20rids,under%20certain%20conditions%2C%20that%20objects%20undergo%20static%20initialization.
+*/
+
 class TCPSocket{
 
 public:
@@ -16,19 +22,33 @@ public:
 
         return 0;
     };
-    
+
+#if 1    
+    //a workaround
+
     TCPSocket(TCPSocket& that){
 
         m_socket_fd = that.m_socket_fd;
         that.m_socket_fd = -1;
     }
+#endif
+
+    TCPSocket(TCPSocket&& rvref) : m_socket_fd(-1) {
+    //p503 in book
+        std::swap(m_socket_fd, rvref.m_socket_fd);
+    }
+
+    TCPSocket(const TCPSocket&) = delete;
+    TCPSocket& operator = (const TCPSocket&) = delete;
 
 private:
 
     int m_socket_fd;
-
-    //TCPSocket(const TCPSocket&);
+#if 0
+    not copyable usage in c++03
+    TCPSocket(const TCPSocket&);
     TCPSocket& operator = (const TCPSocket&);
+#endif
 };
 
 TCPSocket connectToService(){
